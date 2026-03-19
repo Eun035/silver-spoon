268f2eae11bb9a16d03e40efd69928cad0d6dc58
eun035 <eun035@gmail.com>
2026-03-19 09:02:44 +0900

    chore: skip consent screen if already authorized

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -47,7 +47,7 @@ export const authOptions: NextAuthOptions = {
                 params: {
                     scope: "openid email profile https://www.googleapis.com/auth/calendar.events",
                     access_type: "offline",
-                    prompt: "consent",
+                    prompt: "select_account",
                 },
             },
         }),
